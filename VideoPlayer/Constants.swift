dff5a8e04c86b681db388d664d3c5aaa9fe58855
Michael He <hlxwell@gmail.com>
2018-03-05 14:22:03 +0900

    add constants file, so we are able to put all the global constants here.

---
+++ VideoPlayer/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  VideoPlayer
+//
+//  Created by Michael He on 2018/02/27.
+//  Copyright © 2018 Michael He. All rights reserved.
+//
+
+enum Constants {
+    static let videoPlayerRatio = 480.0 / 270.0
+}
