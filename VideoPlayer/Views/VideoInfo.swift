c589e6435cc7f4895b4f896cf6301336af46a719
Michael He <hlxwell@gmail.com>
2018-03-05 14:26:53 +0900

    add video info view & xib

---
+++ VideoPlayer/Views/VideoInfo.swift
@@ -0,0 +1,18 @@
+//
+//  VideoInfo.swift
+//  VideoPlayer
+//
+//  Created by Michael He on 2018/03/01.
+//  Copyright © 2018 Michael He. All rights reserved.
+//
+
+import UIKit
+
+class VideoInfo: UIView {
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+
+    override func awakeFromNib() {
+    }
+}
