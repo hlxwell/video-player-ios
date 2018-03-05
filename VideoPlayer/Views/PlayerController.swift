f945ee49318c19f9358f2bcbe32c089fd6354e2a
Michael He <hlxwell@gmail.com>
2018-03-05 19:30:26 +0900

    1. add progress bar progress synch 2. add rewind and forward function 3. add uiview onTap event, so it can hide when user tap on it. 4. add time display label with observer to update it.

--- VideoPlayer/Views/PlayerController.swift
+++ VideoPlayer/Views/PlayerController.swift
@@ -16,13 +16,28 @@ class PlayerController: UIView {
     @IBOutlet weak var playAndPauseButton: UIButton!
     @IBOutlet weak var progressSlider: UISlider!
     @IBOutlet weak var maxMinButton: UIButton!
+    @IBOutlet weak var durationLabel: UILabel!
 
-    public weak var player: AVPlayer?
+    private weak var _player: AVPlayer?
+
+    var player: AVPlayer {
+        get {
+            return _player!
+        }
+        set {
+            _player = newValue
+            addTimeObserver()
+        }
+    }
 
     override func awakeFromNib() {
     }
 
-    @IBAction func progressChanged(_ sender: Any) {
+    @IBAction func progressChanged(_ sender: UISlider) {
+        guard let duration = _player?.currentItem?.duration else { return }
+
+        let currentTime = Double(sender.value) * duration.seconds
+        _player?.seek(to: CMTimeMake(Int64(currentTime * 1000), 1000))
     }
 
     @IBAction func fullscreen(_ sender: Any) {
@@ -33,23 +48,63 @@ class PlayerController: UIView {
         } else {
             orientation = UIInterfaceOrientation.landscapeLeft.rawValue
         }
-        
+
         UIDevice.current.setValue(orientation, forKey: "orientation")
     }
 
     @IBAction func rewind(_ sender: Any) {
+        let currentTime = CMTimeGetSeconds((_player?.currentTime())!)
+        var newTime = currentTime - 5.0
+        if newTime < 0 { newTime = 0 }
+        _player?.seek(to: CMTimeMake(Int64(newTime * 1000), 1000))
+        updateProgressBar()
     }
 
     @IBAction func forward(_ sender: Any) {
+        guard let duration = _player?.currentItem?.duration else { return }
+
+        let currentTime = CMTimeGetSeconds((_player?.currentTime())!)
+        let newTime = currentTime + 5.0
+        if newTime <= CMTimeGetSeconds(duration) {
+            _player?.seek(to: CMTimeMake(Int64(newTime * 1000), 1000))
+        }
+        updateProgressBar()
     }
 
     @IBAction func playAndPause(_ sender: Any) {
-        if player!.isPlaying {
-            player!.pause()
+        if _player!.isPlaying {
+            _player!.pause()
             playAndPauseButton.setImage(UIImage(named: "PlayButton"), for: .normal)
         } else {
-            player!.play()
+            _player!.play()
             playAndPauseButton.setImage(UIImage(named: "PauseButton"), for: .normal)
         }
+    }
+
+    @IBAction func handleOnTap(_ sender: Any) {
+        alpha = 0.5
+        UIView.animate(withDuration: 0.5, animations: {
+            self.alpha = 0
+        }) { (isComplete) in
+            self.removeFromSuperview()
+        }
+    }
+    
+    // Private Methods -----------------------------------------------------
+
+    private func updateProgressBar() -> Void {
+        guard let duration = _player?.currentItem?.duration.seconds else { return }
+        let currentTime = (_player?.currentTime().seconds)!
+        progressSlider.value = Float(currentTime / duration)
+    }
+    
+    private func addTimeObserver() -> Void {
+        let updateInterval = CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        _player?.addPeriodicTimeObserver(forInterval: updateInterval, queue: DispatchQueue.main, using: { [weak self] time in
+            guard let currentItem = self?._player?.currentItem else { return }
+            let currentTime = Int((self?._player?.currentTime().seconds)! * 1000).convertToDisplayTime()
+            let duration = Int(currentItem.duration.seconds * 1000).convertToDisplayTime()
+            self?.durationLabel.text = String(format: "%@ / %@", currentTime, duration)
+        })
     }
 }
